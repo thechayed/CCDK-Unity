e7f41d781f1c9006afdd9bdbcd63559cb994a904
thechayed <38384626+thechayed@users.noreply.github.com>
2021-10-09 12:32:23 -0400

    update Projectile.cs

--- Engine/Engine/Gameplay Elements/Projectile.cs
+++ Engine/Engine/Gameplay Elements/Projectile.cs
@@ -10,18 +10,10 @@ namespace CCDKGame
         // Motion information.
         /** Initial speed of projectile. */
         public float Speed;
-        /** Limit on speed of projectile (0 means no limit). */
-        public float MaxSpeed;
         /**The angle of motion of the projectile**/
         public Vector3 movingAngle;
 
-        // Damage parameters
-        /** Damage done by the projectile */
-        public float Damage;
-        /** Radius of effect in which damage is applied. */
-        public float DamageRadius;
-        /** Momentum magnitude imparted by impacting projectile. */
-        public float MomentumTransfer;
+
 
         /** The object we collided with**/
         public GameObject ImapactedObject;
